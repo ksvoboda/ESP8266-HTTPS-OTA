abbe0a6e5a7bc189bcb4b8ed17794a135869cd2d
Kryštof Svoboda <39433099+ksvoboda@users.noreply.github.com>
2021-02-01 15:32:15 +0100

    test

--- firmware/firmwareUpdate.h
+++ firmware/firmwareUpdate.h
@@ -43,9 +43,9 @@ void firmwareUpdate() {
   const char* fwVersion = doc["tag_name"];
   String fullDownloadURL = doc["assets"][0]["browser_download_url"];
 
-  Serial.print("New firmware version: ");
+  Serial.print("Latest firmware version: ");
   Serial.println(fwVersion);
-  Serial.print("New firmware download URL: ");
+  Serial.print("Latest firmware download URL: ");
   Serial.println(fullDownloadURL);
 
   //-------------OTA Update-------------//

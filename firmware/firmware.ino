85fbbfb080ba60ff62c80881e0d781d92c06c399
Kryštof Svoboda <39433099+ksvoboda@users.noreply.github.com>
2021-02-28 11:19:52 +0100

    organised

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,11 +1,61 @@
 #include "firmwareUpdate.h"
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+#include <MQTT.h>
+
+const char* mqttServer = "192.168.12.131";
+const int mqttPort = 1883;
+const char* mqttUser = "guest";
+const char* mqttPassword = "guest";
 
 const char* ssid = "curry";
 const char* password = "Papagaj43*";
 
 #define MSG_BUFFER_SIZE  (50)
+
+WiFiClient wifiClient;
+MQTTClient mqttClient;
+
+unsigned long lastMillis = 0;
+
+void connect() {
+  
+  Serial.print("checking wifi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    
+    Serial.print(".");
+    delay(1000);
+    
+  }
+
+  Serial.print("\nconnecting...");
+  while (!mqttClient.connect("ESP-8266", mqttUser, mqttPassword)) {
+    
+    Serial.print(".");
+    delay(1000);
+    
+  }
+
+  Serial.println("\nconnected!");
+
+  mqttClient.subscribe("/devices/update");
+
+}
+
+void messageReceived(String &topic, String &payload) {
+  
+  Serial.println("incoming: " + topic + " - " + payload);
+
+  if (topic == "/devices/update" && payload == "update") {
+    
+    firmwareUpdate();
+  
+  }
+
+  // Note: Do not use the client in the callback to publish, subscribe or
+  // unsubscribe as it may cause deadlocks when other things arrive while
+  // sending and receiving acknowledgments. Instead, change a global variable,
+  // or push to a queue and handle it in the loop after calling `client.loop()`.
+  
+}
 
 void setup() {
 
